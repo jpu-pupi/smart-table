c7bf59aa7dfa88f92b88bb21eb449dbe8179a5e8
Tank <soboleva.tan11@gmail.com>
2025-11-05 18:17:37 +0300

    done

--- src/components/searching.js
+++ src/components/searching.js
@@ -2,10 +2,18 @@ import {rules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
+     // @todo: #5.1 — настроить компаратор
+    const comparator = createComparison([
+        'skipEmptyTargetValues',
+        'searchMultipleFields'
+    ]);
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        const searchValue = state[searchField];
+        return data.filter(item => comparator(item, {
+            [searchField]: searchValue,
+            searchFields: ['date', 'customer', 'seller']
+        }));
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,35 @@
-import './fonts/ys-display/fonts.css'
-import './style.css'
+import "./fonts/ys-display/fonts.css";
+import "./style.css";
 
-import {data as sourceData} from "./data/dataset_1.js";
+import { data as sourceData } from "./data/dataset_1.js";
 
-import {initData} from "./data.js";
-import {processFormData} from "./lib/utils.js";
+import { initData } from "./data.js";
+import { processFormData } from "./lib/utils.js";
 
-import {initTable} from "./components/table.js";
+import { initTable } from "./components/table.js";
+import { initPagination } from "./components/pagination.js";
+
+import { initSearching } from "./components/searching.js";
 // @todo: подключение
 
-
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const { data, ...indexes } = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
+  const state = processFormData(new FormData(sampleTable.container));
+  const rowsPerPage = parseInt(state.rowsPerPage); // приведём количество страниц к числу
+  const page = parseInt(state.page ?? 1); // номер страницы по умолчанию 1 и тоже число
 
-    return {
-        ...state
-    };
+  return {
+    // расширьте существующий return вот так
+    ...state,
+    rowsPerPage,
+    page,
+  };
 }
 
 /**
@@ -30,25 +37,43 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+  let state = collectState(); // состояние полей из таблицы
+  let result = [...data]; // копируем для последующего изменения
+  // @todo: использование
+  result = applySearch(result, state);
+  
+  result = applyPagination(result, state, action);
 
-
-    sampleTable.render(result)
+  sampleTable.render(result);
 }
 
-const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: [],
-    after: []
-}, render);
+const sampleTable = initTable(
+  {
+    tableTemplate: "table",
+    rowTemplate: "row",
+    before: ["search"],
+    after: ["pagination"],
+  },
+  render
+);
 
 // @todo: инициализация
+const applySearch = initSearching('search');
 
+const applyPagination = initPagination(
+  sampleTable.pagination.elements, // передаём сюда элементы пагинации, найденные в шаблоне
+  (el, page, isCurrent) => {
+    // и колбэк, чтобы заполнять кнопки страниц данными
+    const input = el.querySelector("input");
+    const label = el.querySelector("span");
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  }
+);
 
-const appRoot = document.querySelector('#app');
+const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
 render();
