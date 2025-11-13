b371bbe63ec9b4199d2123608e7f4924729f73be
Tank <soboleva.tan11@gmail.com>
2025-11-13 18:44:31 +0300

    filtering w/o applying

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,35 +1,56 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(
+                ...Object.values(indexes[elementName]).map(
+                    (name) => {
+                        const el = document.createElement("option");
+                        el.textContent = name;
+                        el.value = name;
+                        return el;
+                    }
+            )
+            );
+        });
+    };
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
+  const applyFiltering = (query, state, action) => {
+    // код с обработкой очистки поля
+    if (action && action.name === "clear") {
+      const fieldName = action.dataset.field;
 
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                        // Перебираем по именам
-        elements[elementName].append(                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                      .map(name => {                        // используйте name как значение и текстовое содержимое
-                          const option = document.createElement('option');
-                          option.value = name;
-                          option.textContent = name;
-                          return option;
-                      })
-        )
-     });
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-         if (action && action.name === 'clear') {
-            const parent = action.parentElement;
-            const input = parent.querySelector('select, input');
-            const field = action.dataset.field;
+      // Находим поле ввода по имени
+      const input = elements[fieldName];
 
-            if (input) input.value = '';
-            if (field && field in state) {
-                delete state[field];
-            }
-        };
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+      if (input) {
+        // Сбрасываем значение поля ввода
+        input.value = "";
+
+        // Очищаем соответствующее поле в state
+        state[fieldName] = "";
+      }
+      // @todo: #4.5 — отфильтровать данные, используя компаратор
+      const filter = {};
+      Object.keys(elements).forEach((key) => {
+        if (elements[key]) {
+          if (
+            ["INPUT", "SELECT"].includes(elements[key].tagName) &&
+            elements[key].value
+          ) {
+            // ищем поля ввода в фильтре с непустыми данными
+            filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+          }
+        }
+      });
+
+      return Object.keys(filter).length
+        ? Object.assign({}, query, filter)
+        : query; // если в фильтре что-то добавилось, применим к запросу
     }
+  };
+
+  return {
+    updateIndexes,
+    applyFiltering,
+  };
 }
